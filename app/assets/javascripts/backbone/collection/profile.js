ad4e3a7b9aabe7752b6ba1b83394fb424f16fc85
Vibrantlife <nicholesvibrantlife@gmail.com>
2015-05-20 00:39:14 -0700

    created model for college with backbone

--- app/assets/javascripts/backbone/collection/profile.js
+++ app/assets/javascripts/backbone/collection/profile.js
@@ -1,54 +1,57 @@
-var profileTable = Backbone.View.extend({
-  // table for profile
-  el: $('#userProfile'),
-  initialize: function() {
-    this.collection.bind("add", this.render, this);
-  },
+// not rendering email and username to the page. I think I'm querying improperly on the view side. I commented out the code to keep it from running.
 
-  render: function() {
-    _.each(this.collection.models, function(data){
-      this.$el.append(new userProfile({
-        model: data
-      }).render().el);
-    }, this);
-    return this;
-  }
-});
+//var profileTable = Backbone.View.extend({
+//   // table for profile
+//   el: $('#userProfile'),
+//   initialize: function() {
+//     this.collection.bind("add", this.render, this);
+//   },
+
+//   render: function() {
+//     _.each(this.collection.models, function(data){
+//       this.$el.append(new userProfile({
+//         model: data
+//       }).render().el);
+//     }, this);
+//     return this;
+//   }
+// });
 
 
-var User = Backbone.Model.extend({
-});
 
-var userCollection = Backbone.Collection.extend({
-  model: User,
-  url: '/user',
-  parse: function(res) {
-    console.log('response inside parse' + res);
-    return res;
-  }
-})
+// var User = Backbone.Model.extend({
+// });
 
-var user = new userCollection();
+// var userCollection = Backbone.Collection.extend({
+//   model: User,
+//   url: '/user',
+//   parse: function(res) {
+//     console.log('response inside parse' + res);
+//     return res;
+//   }
+// })
 
-user.fetch({
-  success: function() {
-    console.log(user.toJSON());
-    new profileTable({collection: userCollection}.render());
-  },
-  error: function() {
-    console.log('no candy for you');
-  }
-});
+// var user = new userCollection();
 
-// view for user profile
-$(document).ready(function() {
-var userProfile = Backbone.View.extend({
-  tagName: 'tr',
-  template: _.template($('#user-template').html()),
-  render: function() {
-    this.$el.html(this.template(this.model.toJSON()));
-    return this;
-  }
-});
+// user.fetch({
+//   success: function() {
+//     console.log(user.toJSON());
+//     new profileTable({collection: userCollection}.render());
+//   },
+//   error: function() {
+//     console.log('no candy for you');
+//   }
+// });
 
-});
+// // view for user profile
+// $(document).ready(function() {
+// var userProfile = Backbone.View.extend({
+//   tagName: 'tr',
+//   template: _.template($('#profiletemplate').html()),
+//   render: function() {
+//     this.$el.html(this.template(this.model.toJSON()));
+//     return this;
+//   }
+// });
+
+// });
---
+++ app/assets/javascripts/backbone/models/colleges.js
@@ -0,0 +1,8 @@
+var Colleges = Backbone.Model.extend({
+  url: function() {
+    var base = "colleges";
+    if (this.isNew()) return base;
+    return base + (base.charAt(base.length - 1) == '/' ? '' : '/') + this.id;
+    // if it's an unsaved model post to /colleges. if its not new post to /colleges/id to update
+  }
+});
